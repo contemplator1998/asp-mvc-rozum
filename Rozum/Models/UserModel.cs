7626c46044585245b715a3038d2cc3db12101854
ivan.zaykov <ivan.zaykov@rozum.com>
2019-03-20 15:16:47 +0300

    Add login

---
+++ Rozum/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rozum.Models
+{
+    public class User
+    {
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
